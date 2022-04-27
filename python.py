18e4830f14ff7015a88e5bf8652a2d9a1724a3d1
rehemae <rehemaephraimnanda@gmail.com>
2022-04-27 18:08:59 +0300

    python

---
+++ python.py
@@ -0,0 +1,51 @@
+#Strings 
+print("Rehema") 
+x="Am a student at AkiraChix"
+print(len(x))
+#len() use to count the leghth of the statement
+b="Python is the best programming language in the world"
+print(len(b))
+
+c="    "
+print(len(c))
+# Accessing characters
+#the index must be closed by square brackets
+a="anna"
+x=a[0]
+print(x)
+
+k="python"
+j= k[4]
+print(j)
+
+#add
+data1=200
+data2=200
+print(data1+data2)
+
+#sub
+data1=200
+data2=200
+print(data1-data2)
+
+#mut
+float1=30
+float2=4
+print(float1*float2)
+
+#division
+float1=30
+float2=4
+print(float1/float2)
+
+#modules its give the result of the remaining number
+float1=30
+float2=4
+print(float1%float2)
+
+
+
+
+
+
+
